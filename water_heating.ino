5c88b70be718ed7d2350859cfca409df403e58c6
neelshearer <neel.shearer@gmail.com>
2015-05-07 00:09:22 +0100

    Update water_heating.ino
    Changed lots of things:
    now uses motorshield(v2.3) with PWM for motors and stair lights (helps reduce power consumption)
    adds a light sensor to illuminate stair lights
    sets pump speeds for Rocket stove according to need

--- water_heating.ino
+++ water_heating.ino
@@ -1,18 +1,23 @@
-#include <Adafruit_MotorShield.h>
-
 /*********************************************************************
 
+Water_Heating.ino
+
 This is the code for running a Solar hot water system, with backup Rocket 
-stove water heating for the greyer days!
+stove water heating for the greyer days! It also runs a light sensor to turn 
+on campsite stair lighting when it gets dark. It adjusts the level of light as 
+it gets darker to avoid dazzling campers and disturbing animal life too much.
 
 It makes use of several libraries:
 OneWire
 Dallas Temperature
 SPI
 Adafruit Graphics library
+Adafruit Motorshield (v2.3)
+TSL2561 Light Sensor
 
-The system uses 4 onewire temperature sensors, 3 relays, and an Adafruit 1306 
-Monochrome OLED display
+The system uses 4 onewire temperature sensors, all 4 PWM outputs of an Adafruit 
+Motorshield (v2.3), a TSL2561 Light sensor from Sparkfun and an Adafruit 1306 
+Monochrome OLED display.
 
 It should be fairly easy to tweak this to your needs - code is commented, and 
 makes use of only trivial constructs (if, for, do etc) and glyph building 
@@ -27,16 +32,31 @@ Electricity
 Heat
 Pressure
 
-Do your homework, and above all, have fun and good luck!
+Do your homework, design with safety in mind, and build carefully.
+
+Above all, have fun and good luck!
 
 *********************************************************************/
 
-#include <Aduino.h>
+#include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
+#include <SFE_TSL2561.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Create an SFE_TSL2561 object, here called "light":
+
+SFE_TSL2561 light;
+
+// Global variables:
+
+boolean gain;     // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms;  // Integration ("shutter") time in milliseconds
  
 // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
@@ -44,7 +64,7 @@ Do your homework, and above all, have fun and good luck!
 // Declare this variable
 // TODO: Not sure if this is required - looks like it isn't - only other ref 
 //  is near line 147
-int testRoutine = 0; 
+//int testRoutine = 0; 
 
 // This section disabled to all use with Motorshield v2.0
 // Declare relay variables
@@ -60,6 +80,7 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *motorTank = AFMS.getMotor(1);
 Adafruit_DCMotor *motorSolar = AFMS.getMotor(2);
 Adafruit_DCMotor *motorRocket = AFMS.getMotor(3);
+Adafruit_DCMotor *stairLights = AFMS.getMotor(4);
  
 // Setup a oneWire instance to communicate with any OneWire devices 
 // (not just Maxim/Dallas temperature ICs)
@@ -67,11 +88,6 @@ OneWire oneWire(ONE_WIRE_BUS);
  
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
 // If using software SPI (the default case):
 #define OLED_MOSI   9
@@ -88,10 +104,10 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 */
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
+//#define NUMFLAKES 10
+//#define XPOS 0
+//#define YPOS 1
+//#define DELTAY 2
 
 // Declare the glyph for the Tank loop 
 
@@ -168,25 +184,53 @@ static const unsigned char PROGMEM logo16_rock_bmp[] =
 
 void setup()
 {
+  
+// start serial port
+//  Serial.begin(9600);
+
+  light.begin();
+  
+  // The light sensor has a default integration time of 402ms,
+  // and a default gain of low (1X).
+  
+  // If you would like to change either of these, you can
+  // do so using the setTiming() command.
+  
+  // If gain = false (0), device is set to low gain (1X)
+  // If gain = high (1), device is set to high gain (16X)
+
+  gain = 0;
+
+  // If time = 0, integration will be 13.7ms
+  // If time = 1, integration will be 101ms
+  // If time = 2, integration will be 402ms
+  // If time = 3, use manual start / stop to perform your own integration
+
+  unsigned char time = 2;
+
+  // setTiming() will set the third parameter (ms) to the
+  // requested integration time in ms (this will be useful later):
+  
+  light.setTiming(gain,time,ms);
+
+  // To start taking measurements, power up the sensor:
+  
+  light.setPowerUp();
+  
 // initialize the digital pins for Relays as outputs.
-  pinMode(relayTank, OUTPUT);
-  pinMode(relaySolar, OUTPUT);
-  pinMode(relayRocket, OUTPUT);
+//  pinMode(relayTank, OUTPUT);
+//  pinMode(relaySolar, OUTPUT);
+//  pinMode(relayRocket, OUTPUT);
 
 // initialize this Input pin
 // TODO: Do we need this?
-  pinMode(testRoutine, INPUT);
+//  pinMode(testRoutine, INPUT);
 
 // Start with Relays turned off (state:HIGH) apart from Rocket, which default 
 //  to on (state:LOW)
-  digitalWrite(relayTank, HIGH);
-  digitalWrite(relaySolar, HIGH);
-  digitalWrite(relayRocket, LOW);
-
-  
-  // start serial port
-  // TODO: Do we actually need this?
-  Serial.begin(9600);
+//  digitalWrite(relayTank, HIGH);
+//  digitalWrite(relaySolar, HIGH);
+//  digitalWrite(relayRocket, LOW);
 
   // Start up the library
   sensors.begin();
@@ -196,12 +240,80 @@ void setup()
   display.begin(SSD1306_SWITCHCAPVCC);
   // init done
   display.clearDisplay();
+
+  motorTank->setSpeed(255);
+  motorSolar->setSpeed(255);
+  motorRocket->setSpeed(255);
+  motorTank->run(FORWARD);  // turn on motor
+  motorSolar->run(FORWARD);  // turn on motor
+  motorRocket->run(FORWARD);  // turn on motor
+  delay(1000);
+  motorTank->run(RELEASE);  // turn off motor
+  motorSolar->run(RELEASE);  // turn off motor
+  motorRocket->run(RELEASE);  // turn off motor
+
   
 // End setup loop
   }
 
 void loop(void)
 {
+  uint8_t i;
+  AFMS.begin(20);  // create with the default frequency 1.6KHz
+  
+  // There are two light sensors on the device, one for visible light
+  // and one for infrared. Both sensors are needed for lux calculations.
+  
+  // Retrieve the data from the device:
+
+  unsigned int data0, data1;
+  
+  if (light.getData(data0,data1))
+  {
+    // getData() returned true, communication was successful
+    
+ 
+    // To calculate lux, pass all your settings and readings
+    // to the getLux() function.
+    
+    // The getLux() function will return 1 if the calculation
+    // was successful, or 0 if one or both of the sensors was
+    // saturated (too much light). If this happens, you can
+    // reduce the integration time and/or gain.
+    // For more information see the hookup guide at: https://learn.sparkfun.com/tutorials/getting-started-with-the-tsl2561-luminosity-sensor
+  
+    double lux;    // Resulting lux value
+    boolean good;  // True if neither sensor is saturated
+    
+    // Perform lux calculation:
+
+    good = light.getLux(gain,ms,data0,data1,lux);
+    
+    // Print out the results:
+	
+//    Serial.print("lux: ");
+//    Serial.println(lux);
+//    if (good) Serial.println(" (good)"); else Serial.println(" (BAD)");
+    if (lux < 40) 
+    {
+      stairLights->setSpeed((lux*3)+20);
+      stairLights->run(FORWARD);
+    }
+    else
+    {
+      stairLights->setSpeed(0);
+      stairLights->run(RELEASE);
+    }
+  }
+  else
+  {
+    // getData() returned false because of an I2C error, inform the user.
+
+    byte error = light.getError();
+    printError(error);
+  }
+
+
 // call sensors.requestTemperatures() to issue a global temperature
 // request to all devices on the bus
  sensors.requestTemperatures(); 
@@ -221,11 +333,17 @@ void loop(void)
 
 if (tempLLH>(tempTank+5))
 {
-digitalWrite(relayTank, LOW);
+  motorTank->run(FORWARD);
+  for (i=255; i>50; i=i-10) {
+    motorTank->setSpeed(i);  
+    delay(10);
+  }
+    motorTank->setSpeed(50);  
 }
 else
 {
-digitalWrite(relayTank, HIGH);
+    motorTank->setSpeed(0);  
+    motorTank->run(RELEASE);
 };
 
 //
@@ -234,11 +352,17 @@ digitalWrite(relayTank, HIGH);
 
 if (tempSolar>(tempLLH+10))
 {
-digitalWrite(relaySolar, LOW);
+  motorSolar->run(FORWARD);
+  for (i=255; i>50; i=i-10) {
+    motorSolar->setSpeed(i);  
+    delay(10);
+  }
+    motorSolar->setSpeed(50);  
 }
 else
 {
-digitalWrite(relaySolar, HIGH);
+    motorSolar->setSpeed(0);  
+    motorSolar->run(RELEASE);
 };
 
 //
@@ -247,11 +371,29 @@ digitalWrite(relaySolar, HIGH);
 
 if (tempRocket>(tempLLH+10))
 {
-digitalWrite(relayRocket, LOW);
+  motorRocket->run(FORWARD);
+  for (i=255; i>50; i=i-10) {
+    motorRocket->setSpeed(i);  
+    delay(10);
+  }
+  if (tempRocket<=65)
+  {
+    motorRocket->setSpeed(50);
+  }
+  else if (tempRocket>=90)
+  {
+    motorRocket->setSpeed(255);
+  }
+  else
+  {
+    motorRocket->setSpeed(round(((tempRocket-65)*8.2)+50));
+  }
+     
 }
 else
 {
-digitalWrite(relayRocket, HIGH);
+    motorRocket->setSpeed(0);  
+    motorRocket->run(RELEASE);
 };
 
 // Sanity check for Tank temperature higher than Solar or Rocket to be sure
@@ -260,12 +402,14 @@ digitalWrite(relayRocket, HIGH);
 
 if (tempTank > tempRocket)
 {
-digitalWrite(relayRocket, HIGH);
+    motorRocket->setSpeed(0);  
+    motorRocket->run(RELEASE);
 };
 
 if (tempTank > tempSolar)
 {
-digitalWrite(relaySolar, HIGH);
+    motorSolar->setSpeed(0);  
+    motorSolar->run(RELEASE);
 };
 
 //
@@ -274,7 +418,9 @@ digitalWrite(relaySolar, HIGH);
 //  state (can happen if there is a bad connection to a sensor)
 // Error state of the onewire temp sensor is 85 degrees, however, I have also 
 //  seen temps of -127 reported: check for both and declare an error state
-// Error state is the activation of all pumps for safety reasons.
+// Error state declared when the rocket stove is reporting a temperature above 
+//  90degrees
+// Error state is the activation of all pumps at high speed for safety reasons.
 // TODO: Even in an error state, allow pumps to rest for 30 seconds in every 5
 //  minutes
 //
@@ -292,11 +438,15 @@ if (tempTank==-127.00 ||
     tempTank==tempRocket || 
     tempLLH==tempSolar || 
     tempLLH==tempRocket || 
-    tempSolar==tempRocket)
+    tempSolar==tempRocket ||
+    tempRocket>=90.00)
 {
-digitalWrite(relayTank, LOW);
-digitalWrite(relaySolar, LOW);
-digitalWrite(relayRocket, LOW);
+  motorTank->setSpeed(255);
+  motorSolar->setSpeed(255);
+  motorRocket->setSpeed(255);
+  motorTank->run(FORWARD);  // turn on motor
+  motorSolar->run(FORWARD);  // turn on motor
+  motorRocket->run(FORWARD);  // turn on motor
 
 // Briefly display current error state
 
@@ -355,7 +505,7 @@ digitalWrite(relayRocket, LOW);
   display.print("Water Temp.");
   display.display();
  
-  delay(6000);
+  delay(5000);
 
 //
 // Show all temperatures as reported (2 decimal places)
@@ -382,9 +532,9 @@ digitalWrite(relayRocket, LOW);
 // Detect if any pumps are currently running
 //
 
-if ((digitalRead(relayTank) == LOW) || 
-    (digitalRead(relaySolar) == LOW) || 
-    (digitalRead(relayRocket) == LOW))
+if ((tempLLH>(tempTank+5)) || 
+    (tempSolar>(tempLLH+5)) || 
+    (tempRocket>(tempLLH+5)))
 {
 
 // Animated display routine
@@ -402,15 +552,15 @@ if ((digitalRead(relayTank) == LOW) ||
   display.write(9);
   display.println("C");
   display.setTextSize(2);
-if (digitalRead(relayTank) == LOW){
+if (tempLLH>(tempTank+5)){
   display.setCursor(20,48);
   display.print("/");
 };
-if (digitalRead(relaySolar) == LOW){
+if (tempSolar>(tempLLH+5)){
   display.setCursor(64,48);
   display.print("-");
 };
-if (digitalRead(relayRocket) == LOW){
+if (tempRocket>(tempLLH+5)){
   display.setCursor(112,48);
   display.print("\\");
 };
@@ -430,15 +580,15 @@ if (digitalRead(relayRocket) == LOW){
   display.write(9);
   display.println("C");
   display.setTextSize(2);
-if (digitalRead(relayTank) == LOW){
+if (tempLLH>(tempTank+5)){
   display.setCursor(20,48);
   display.print("-");
 };
-if (digitalRead(relaySolar) == LOW){
+if (tempSolar>(tempLLH+5)){
   display.setCursor(64,48);
   display.print("\\");
 };
-if (digitalRead(relayRocket) == LOW){
+if (tempRocket>(tempLLH+5)){
   display.setCursor(112,48);
   display.print("|");
 };
@@ -458,15 +608,15 @@ if (digitalRead(relayRocket) == LOW){
   display.write(9);
   display.println("C");
   display.setTextSize(2);
-if (digitalRead(relayTank) == LOW){
+if (tempLLH>(tempTank+5)){
   display.setCursor(20,48);
   display.print("\\");
 };
-if (digitalRead(relaySolar) == LOW){
+if (tempSolar>(tempLLH+5)){
   display.setCursor(64,48);
   display.print("|");
 };
-if (digitalRead(relayRocket) == LOW){
+if (tempRocket>(tempLLH+5)){
   display.setCursor(112,48);
   display.print("/");
 };
@@ -486,15 +636,15 @@ if (digitalRead(relayRocket) == LOW){
   display.write(9);
   display.println("C");
   display.setTextSize(2);
-if (digitalRead(relayTank) == LOW){
+if (tempLLH>(tempTank+5)){
   display.setCursor(20,48);
   display.print("|");
 };
-if (digitalRead(relaySolar) == LOW){
+if (tempSolar>(tempLLH+5)){
   display.setCursor(64,48);
   display.print("/");
 };
-if (digitalRead(relayRocket) == LOW){
+if (tempRocket>(tempLLH+5)){
   display.setCursor(112,48);
   display.print("-");
 };
@@ -529,3 +679,34 @@ bailloop:
 // End Main loop
 ;
 };
+
+void printError(byte error)
+  // If there's an I2C error, this function will
+  // print out an explanation.
+{
+//  Serial.print("I2C error: ");
+//  Serial.print(error,DEC);
+//  Serial.print(", ");
+  
+  switch(error)
+  {
+    case 0:
+//      Serial.println("success");
+      break;
+    case 1:
+//      Serial.println("data too long for transmit buffer");
+      break;
+    case 2:
+//      Serial.println("received NACK on address (disconnected?)");
+      break;
+    case 3:
+//      Serial.println("received NACK on data");
+      break;
+    case 4:
+//      Serial.println("other error");
+      break;
+    default:
+//      Serial.println("unknown error")
+      ;
+  }
+}
